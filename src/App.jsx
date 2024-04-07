7386100544da2c9c1095f4fc069f800886b14e93
Slinkter <luis.j.cueva@gmail.com>
2024-04-07 15:49:03 -0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@material-tailwind/react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-} from "@material-tailwind/react";
+import { Typography } from "@material-tailwind/react";
+
+const BASE_URL_API = "https://api.thecatapi.com/v1/images";
+const LIMITE_CAT = 5;
+const API_KEY =
+  "live_BgeabuZRHRH2irUsFWjZREQBJ38KmhA2OdWWkOycJQLQ54j44JApcrWGIqXZn9Ym";
+const URL_CAT = `${BASE_URL_API}/search?limit=${LIMITE_CAT}&api_key=${API_KEY}`;
 
 const App = () => {
+  const [cats, setCats] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const getData = async (urlrandom) => {
+    setLoading(true);
+    try {
+      const res = await fetch(urlrandom);
+      const data = await res.json();
+      console.log(data);
+      setCats(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    getData(URL_CAT);
+  }, []);
+
+  const handleButton = () => {
+    console.log("button");
+    getData(URL_CAT);
+  };
+
   console.log("APP");
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Card className="mt-6 w-96">
-        <CardHeader color="blue-gray" className="relative h-56">
-          <img
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-            alt="card-image"
-          />
-        </CardHeader>
-        <CardBody>
-          <Typography variant="h5" color="blue-gray" className="mb-2">
-            UI/UX Review Check
-          </Typography>
-          <Typography>
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-          </Typography>
-        </CardBody>
-        <CardFooter className="pt-0">
-          <Button>Read More</Button>
-        </CardFooter>
-      </Card>
-      <Button>Button</Button>
+    <div className="bg-gradient-to-r from-blue-800 to-indigo-900   h-screen flex  flex-col justify-center items-center">
+      {loading ? (
+        <div className="animate-pulse ">
+          <div className="m-1 rounded-lg shadow-lg bg-cover bg-center bg-gray-300 h-32 w-32 "></div>
+          <div className="m-1 rounded-lg shadow-lg bg-cover bg-center bg-gray-300 h-32 w-32  "></div>
+          <div className="m-1 rounded-lg shadow-lg bg-cover bg-center bg-gray-300 h-32 w-32  "></div>
+          <div className="m-1 rounded-lg shadow-lg bg-cover bg-center bg-gray-300 h-32 w-32  "></div>
+          <div className="m-1 rounded-lg shadow-lg bg-cover bg-center bg-gray-300 h-32 w-32  "></div>
+        </div>
+      ) : (
+        cats.map((cat) => (
+          <div key={cat.id} className="m-1">
+            <img
+              src={cat.url}
+              alt={cat.id}
+              className="rounded-lg shadow-lg bg-cover bg-center h-32 w-32 hover:scale-125 duration-200"
+            />
+          </div>
+        ))
+      )}
+
+      <Button className="mt-3" onClick={() => handleButton()}>
+        get Cat
+      </Button>
     </div>
   );
 };
