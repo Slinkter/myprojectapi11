4f588a1aaaf1b8d0c866002b3bfb870d19aad624
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-10-22 17:43:38 -0500

    estudiando
    estudiando

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,23 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider as MaterialThemeProvider } from "@material-tailwind/react";
+/*  */
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+/*  */
+import { AppThemeProvider } from "./context/ThemeContext";
+import { AppFontProvider } from "./context/FontContext.jsx";
+/*  */
 import App from "./App.jsx";
 import "./index.css";
 
-import { ThemeProvider as MaterialThemeProvider } from "@material-tailwind/react";
-import { Provider } from "react-redux";
-import store from "./redux/store.js";
-import { AppThemeProvider } from "./context/ThemeContext";
-import { AppFontProvider } from "./context/FontContext.jsx";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
+    <MaterialThemeProvider>
         <Provider store={store}>
-            <MaterialThemeProvider>
-                <AppThemeProvider>
-                    <AppFontProvider>
-                        <App />
-                    </AppFontProvider>
-                </AppThemeProvider>
-            </MaterialThemeProvider>
+            <AppThemeProvider>
+                <AppFontProvider>
+                    <App />
+                </AppFontProvider>
+            </AppThemeProvider>
         </Provider>
-    </React.StrictMode>
+    </MaterialThemeProvider>
 );
